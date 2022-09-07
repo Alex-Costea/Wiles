fe6e5c3d666f894690c0d4683553151f011b9293
Alex Costea <alex@costea.in>
2022-09-07 15:16:52 +0300

    add C# project

---
+++ Wiles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Wiles
+{
+    public class Wiles
+    {
+        public static string WilesToCSharp(string code)
+        {
+            return "";
+        }
+    }
+}
+
