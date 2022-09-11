94ad1fbff426e943cb490643ade05fb641468877
Alex Costea <alex@costea.in>
2022-09-11 21:45:32 +0300

    update junit

--- src/main/java/in/costea/wiles/Main.java
+++ src/main/java/in/costea/wiles/Main.java
@@ -9,8 +9,6 @@ import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Main {
-
-
     public static void main(String[] args) {
         ClassLoader classloader = Thread.currentThread().getContextClassLoader();
         try(InputStream is = classloader.getResourceAsStream("input.wiles")) {
@@ -30,6 +28,4 @@ public class Main {
         List<String> tokens= new TokensConverter(input).convert();
         System.out.println(tokens);
     }
-
-
 }
