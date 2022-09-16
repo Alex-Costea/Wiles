fc3819449a96aa6b44b826042aa2a03bbf2b015d
Alex Costea <alex@costea.in>
2022-09-16 11:40:47 +0300

    refactor exceptions

--- src/test/java/SyntaxTreeConverterTests.java
+++ src/test/java/SyntaxTreeConverterTests.java
@@ -1,5 +1,7 @@
+import in.costea.wiles.commands.SyntaxTree;
 import in.costea.wiles.converters.TokensToSyntaxTreeConverter;
 import in.costea.wiles.data.CompilationExceptionsCollection;
+import in.costea.wiles.exceptions.CompilationException;
 import in.costea.wiles.exceptions.TokenExpectedException;
 import in.costea.wiles.exceptions.UnexpectedEndException;
 import org.junit.jupiter.api.Test;
@@ -9,83 +11,104 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import java.util.Arrays;
 
-public class SyntaxTreeConverterTests {
-    public TokensToSyntaxTreeConverter createConverter(String... tokens)
+class CreateConverter
+{
+    TokensToSyntaxTreeConverter converter;
+    CompilationExceptionsCollection exceptions;
+    SyntaxTree tree;
+    public CreateConverter(String... tokens)
     {
-        var temp = new TokensToSyntaxTreeConverter(Arrays.stream(tokens).map(Token::new).toList());
-        temp.convert();
-        return temp;
+        converter = new TokensToSyntaxTreeConverter(Arrays.stream(tokens).map(Token::new).toList());
+        tree =converter.convert();
+        exceptions=converter.getExceptions();
+    }
+    public CompilationExceptionsCollection getExceptions() {
+        return exceptions;
     }
 
+    public String getResult() {
+        return tree.toString();
+    }
+}
+
+public class SyntaxTreeConverterTests {
     @Test
     void emptyBodyTest()
     {
         //Not yet implemented
-        assertThrows(Error.class, this::createConverter);
-        assertThrows(Error.class,()->createConverter(NEWLINE_ID));
+        assertThrows(Error.class, CreateConverter::new);
+        assertThrows(Error.class,()->new CreateConverter(NEWLINE_ID));
     }
-    @Test
-    public void methodDeclarationExceptionsTest()
-    {
-        TokensToSyntaxTreeConverter converter;
-        CompilationExceptionsCollection exceptions=new CompilationExceptionsCollection();
 
-        converter = createConverter(
+    public void assertResults(CompilationExceptionsCollection exceptions, String expectedResult, String... tokens)
+    {
+        CreateConverter converter=new CreateConverter(tokens);
+        if(exceptions==null)
+            exceptions=new CompilationExceptionsCollection();
+        assertEquals(exceptions,converter.getExceptions());
+        if(expectedResult!=null)
+            assertEquals(expectedResult, converter.getResult());
+    }
+
+    @Test
+    public void methodDeclarationTest()
+    {
+        assertResults(null, "PROGRAM(METHOD a (METHOD_BODY))",
                 DECLARE_METHOD_ID,"!a",ROUND_BRACKET_START_ID,ROUND_BRACKET_END_ID
                 ,START_BLOCK_ID,END_BLOCK_ID);
-        assertEquals(exceptions,converter.getExceptions());
 
-        converter = createConverter(
+        assertResults(null, "PROGRAM(METHOD a (METHOD_BODY))",
                 NEWLINE_ID,NEWLINE_ID, DECLARE_METHOD_ID,"!a",ROUND_BRACKET_START_ID,ROUND_BRACKET_END_ID
                 ,START_BLOCK_ID,END_BLOCK_ID);
-        assertEquals(exceptions,converter.getExceptions());
 
-        converter = createConverter(
+        assertResults(null, "PROGRAM(METHOD a (METHOD_BODY))",
                 DECLARE_METHOD_ID,"!a",ROUND_BRACKET_START_ID,
                 NEWLINE_ID,ROUND_BRACKET_END_ID,
                 NEWLINE_ID,START_BLOCK_ID,
                 NEWLINE_ID,END_BLOCK_ID);
-        assertEquals(exceptions,converter.getExceptions());
 
-        converter = createConverter(
+        assertResults(null, "PROGRAM(METHOD a (METHOD_BODY(IDENTIFIER !b ; IDENTIFIER !c )))",
+                DECLARE_METHOD_ID,"!a",ROUND_BRACKET_START_ID,ROUND_BRACKET_END_ID
+                ,START_BLOCK_ID,"!b","!c",END_BLOCK_ID);
+    }
+
+    private CompilationExceptionsCollection createExceptions(CompilationException... list)
+    {
+        CompilationExceptionsCollection exceptions=new CompilationExceptionsCollection();
+        exceptions.add(list);
+        return exceptions;
+    }
+
+    @Test
+    public void programExceptionsTest()
+    {
+        assertResults(createExceptions(new TokenExpectedException("Expected line end!",null)),
+                null,
                 DECLARE_METHOD_ID,"!a",ROUND_BRACKET_START_ID,ROUND_BRACKET_END_ID
                 ,START_BLOCK_ID,END_BLOCK_ID,"!b");
-        exceptions=new CompilationExceptionsCollection();
-        exceptions.add(new TokenExpectedException("Expected line end!",null));
-        assertEquals(exceptions,converter.getExceptions());
 
-        converter = createConverter(
+        assertResults(createExceptions(new TokenExpectedException("Token \"begin\" expected!",null),
+                        new TokenExpectedException("Token \"method\" expected!",null)),
+                null,
                 DECLARE_METHOD_ID,"!a",ROUND_BRACKET_START_ID,ROUND_BRACKET_END_ID,
                 END_BLOCK_ID,NEWLINE_ID,"!c","!d",NEWLINE_ID,"!e");
-        exceptions=new CompilationExceptionsCollection();
-        exceptions.add(new TokenExpectedException("Token \"begin\" expected!",null),
-                new TokenExpectedException("Token \"method\" expected!",null));
-        assertEquals(exceptions,converter.getExceptions());
 
-        converter = createConverter(
+        assertResults(createExceptions(new UnexpectedEndException("Missing token: \"end\"")),
+                null,
                 DECLARE_METHOD_ID,"!a",ROUND_BRACKET_START_ID,ROUND_BRACKET_END_ID,START_BLOCK_ID);
-        exceptions=new CompilationExceptionsCollection();
-        exceptions.add(new UnexpectedEndException("Missing token: \"end\""));
-        assertEquals(exceptions,converter.getExceptions());
 
-        converter = createConverter(
+        assertResults(createExceptions(new UnexpectedEndException("Missing token: \")\"")),
+                null,
                 DECLARE_METHOD_ID,"!a",ROUND_BRACKET_START_ID);
-        exceptions=new CompilationExceptionsCollection();
-        exceptions.add(new UnexpectedEndException("Missing token: \")\""));
-        assertEquals(exceptions,converter.getExceptions());
 
-        converter = createConverter(
+        assertResults(createExceptions(new UnexpectedEndException("Expected method name!")),
+                null,
                 DECLARE_METHOD_ID);
-        exceptions=new CompilationExceptionsCollection();
-        exceptions.add(new UnexpectedEndException("Expected method name!"));
-        assertEquals(exceptions,converter.getExceptions());
 
-        converter = createConverter(
+        assertResults(createExceptions(new UnexpectedEndException("Token \"begin\" expected!"),
+                        new UnexpectedEndException("Token \"begin\" expected!")),
+                null,
                 DECLARE_METHOD_ID,"!name",ROUND_BRACKET_START_ID,ROUND_BRACKET_END_ID, FINISH_STATEMENT,
                 DECLARE_METHOD_ID,"!name",ROUND_BRACKET_START_ID,ROUND_BRACKET_END_ID, FINISH_STATEMENT);
-        exceptions=new CompilationExceptionsCollection();
-        exceptions.add(new UnexpectedEndException("Token \"begin\" expected!"),
-                new UnexpectedEndException("Token \"begin\" expected!"));
-        assertEquals(exceptions,converter.getExceptions());
     }
 }
