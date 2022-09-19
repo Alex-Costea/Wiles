b3cff9e45b884bdb2eb1e8e7fbe224e777eaf168
Alex Costea <alex@costea.in>
2022-09-19 16:54:30 +0300

    exceptions bug fixes

--- src/main/java/in/costea/wiles/commands/OperationCommand.java
+++ src/main/java/in/costea/wiles/commands/OperationCommand.java
@@ -3,7 +3,6 @@ package in.costea.wiles.commands;
 import in.costea.wiles.data.CompilationExceptionsCollection;
 import in.costea.wiles.data.Token;
 import in.costea.wiles.exceptions.CompilationException;
-import in.costea.wiles.exceptions.TokenExpectedException;
 import in.costea.wiles.exceptions.UnexpectedEndException;
 import in.costea.wiles.exceptions.UnexpectedTokenException;
 import in.costea.wiles.services.TokenTransmitter;
@@ -60,62 +59,78 @@ public class OperationCommand extends AbstractOperationComponent
     {
         try
         {
+            //verifying the first token
             Token token = firstToken;
-            if (components.get(0) instanceof TokenCommand firstComponent)
+            String content = token.content();
+            if (content.equals(ROUND_BRACKET_START_ID))
             {
-                if (firstComponent.getContent().equals(ROUND_BRACKET_START_ID))
-                {
-                    components.remove(0);
-                    addInnerOperation();
-                    expectOperatorNext = true;
-                }
-                if (firstComponent.getContent().equals(ROUND_BRACKET_END_ID))
-                {
-                    throw new UnexpectedTokenException("Parentheses must have body!", firstComponent.getToken().location());
-                }
+                components.remove(0);
+                addInnerOperation();
+                expectOperatorNext = true;
             }
+            if(content.equals(FINISH_STATEMENT))
+                throw new UnexpectedTokenException(";",token.location());
+            if (content.equals(ROUND_BRACKET_END_ID))
+                throw new UnexpectedTokenException("Parentheses must have body!", token.location());
+
+            //verifying other tokens
             while (!transmitter.tokensExhausted())
             {
                 token = transmitter.requestToken("");
-                String content = token.content();
+                content = token.content();
+
                 if (content.equals(END_BLOCK_ID) && !innerOperation) //method end statement
                     break;
-                if(content.equals(END_BLOCK_ID)) //end statement in inner operation
-                    throw new UnexpectedTokenException("end",token.location());
+
+                if(content.equals(END_BLOCK_ID))//end statement in inner operation
+                {
+                    transmitter.removeToken();
+                    throw new UnexpectedTokenException("end", token.location());
+                }
+
                 if (expectOperatorNext && !innerOperation && (content.equals(FINISH_STATEMENT) || content.equals(NEWLINE_ID)))
                     break; //finalize operation
+
                 if(content.equals(FINISH_STATEMENT))
-                    throw new UnexpectedTokenException(";",token.location());
+                {
+                    transmitter.removeToken();
+                    throw new UnexpectedTokenException(";", token.location());
+                }
+
                 if(expectOperatorNext)
                     token = expect(x -> OPERATORS.containsValue(x) || x.equals(ROUND_BRACKET_START_ID)
                             || x.equals(ROUND_BRACKET_END_ID), "Operator expected!");
                 else
                     token = expect(x -> x.equals(ROUND_BRACKET_START_ID) || x.equals(ROUND_BRACKET_END_ID) ||
                             x.startsWith("!") || x.startsWith("@") || x.startsWith("#"), "Identifier expected!");
+
                 if (token.content().equals(ROUND_BRACKET_END_ID))
                 {
                     if (innerOperation) break; //end of inner statement
                     else throw new UnexpectedTokenException("Extra parentheses found", token.location());
                 }
+
                 if (expectOperatorNext && token.content().equals(ROUND_BRACKET_START_ID))
                     //TODO: implement
                     throw new Error("Method call not yet implemented!");
+
                 expectOperatorNext=!expectOperatorNext;
+
                 if (token.content().equals(ROUND_BRACKET_START_ID)) //inner operation, not method call
                     addInnerOperation();
                 else components.add(new TokenCommand(transmitter, token));
             }
 
+            //verifying operation finished well
             if (innerOperation && exceptions.size() == 0 &&  !token.content().equals(ROUND_BRACKET_END_ID))
-                exceptions.add(new UnexpectedEndException("Closing parentheses expected", token.location()));
+                throw new UnexpectedEndException("Closing parentheses expected", token.location());
             if (!expectOperatorNext && exceptions.size() == 0)
-                exceptions.add(new UnexpectedEndException("Operation unfinished!", token.location()));
+                throw new UnexpectedEndException("Operation unfinished!", token.location());
+
             //TODO: process order of operations and check if effect exists
         } catch (CompilationException ex)
         {
             exceptions.add(ex);
-            if (ex instanceof TokenExpectedException)
-                transmitter.removeToken();
         }
         return exceptions;
     }
