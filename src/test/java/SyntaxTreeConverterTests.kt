46d0fa9f13598e1e71e3510e188862f1194c74f5
Alex Costea <alex@costea.in>
2022-10-12 12:31:26 +0300

    finished fixing tests

--- src/test/java/SyntaxTreeConverterTests.kt
+++ src/test/java/SyntaxTreeConverterTests.kt
@@ -11,20 +11,20 @@ import `in`.costea.wiles.statics.Constants.ASSIGN_ID
 import `in`.costea.wiles.statics.Constants.COLON_ID
 import `in`.costea.wiles.statics.Constants.COMMA_ID
 import `in`.costea.wiles.statics.Constants.DECLARE_ID
-import `in`.costea.wiles.statics.Constants.METHOD_ID
 import `in`.costea.wiles.statics.Constants.DO_ID
 import `in`.costea.wiles.statics.Constants.END_BLOCK_ID
+import `in`.costea.wiles.statics.Constants.METHOD_ID
 import `in`.costea.wiles.statics.Constants.MINUS_ID
 import `in`.costea.wiles.statics.Constants.NEWLINE_ID
 import `in`.costea.wiles.statics.Constants.NOTHING_ID
 import `in`.costea.wiles.statics.Constants.PLUS_ID
 import `in`.costea.wiles.statics.Constants.POWER_ID
+import `in`.costea.wiles.statics.Constants.RIGHT_ARROW_ID
 import `in`.costea.wiles.statics.Constants.ROUND_BRACKET_END_ID
 import `in`.costea.wiles.statics.Constants.ROUND_BRACKET_START_ID
 import `in`.costea.wiles.statics.Constants.SQUARE_BRACKET_END_ID
 import `in`.costea.wiles.statics.Constants.SQUARE_BRACKET_START_ID
 import `in`.costea.wiles.statics.Constants.START_BLOCK_ID
-import `in`.costea.wiles.statics.Constants.STATEMENT_TERMINATOR_ID
 import `in`.costea.wiles.statics.Constants.TIMES_ID
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
@@ -96,84 +96,68 @@ class SyntaxTreeConverterTests {
 
     @Test
     fun parenthesesTests() {
-        assertResults(null, "PROGRAM(METHOD main(TYPE NOTHING; CODE_BLOCK(EXPRESSION(!a; PLUS; EXPRESSION ROUND(EXPRESSION ROUND(!b; PLUS; !c); PLUS; !d)))))",
+        assertResults(null, "CODE_BLOCK(EXPRESSION(!a; PLUS; EXPRESSION ROUND(EXPRESSION ROUND(!b; PLUS; !c); PLUS; !d)))",
                 "!a", PLUS_ID, ROUND_BRACKET_START_ID, ROUND_BRACKET_START_ID, "!b", PLUS_ID, "!c",
                 ROUND_BRACKET_END_ID, PLUS_ID, "!d", ROUND_BRACKET_END_ID)
-        assertResults(null, "PROGRAM(METHOD main(TYPE NOTHING; CODE_BLOCK(EXPRESSION(!a; PLUS; EXPRESSION ROUND(!b; PLUS; !c)))))",
+        assertResults(null, "CODE_BLOCK(EXPRESSION(!a; PLUS; EXPRESSION ROUND(!b; PLUS; !c)))",
                 "!a", PLUS_ID, ROUND_BRACKET_START_ID, NEWLINE_ID, "!b", PLUS_ID, "!c", ROUND_BRACKET_END_ID)
-        assertResults(null, "PROGRAM(METHOD main(TYPE NOTHING; CODE_BLOCK(EXPRESSION(!a; PLUS; EXPRESSION ROUND(!b; PLUS; !c)))))",
+        assertResults(null, "CODE_BLOCK(EXPRESSION(!a; PLUS; EXPRESSION ROUND(!b; PLUS; !c)))",
                 "!a", PLUS_ID, NEWLINE_ID, ROUND_BRACKET_START_ID, "!b", PLUS_ID, "!c", ROUND_BRACKET_END_ID)
-        assertResults(null, "PROGRAM(METHOD main(TYPE NOTHING; CODE_BLOCK(EXPRESSION ROUND(!a; PLUS; !b))))",
-                ROUND_BRACKET_START_ID, "!a", PLUS_ID, "!b", ROUND_BRACKET_END_ID)
+        assertResults(null, "CODE_BLOCK(EXPRESSION(!a; PLUS; !b))",
+            ROUND_BRACKET_START_ID, "!a", PLUS_ID, "!b", ROUND_BRACKET_END_ID)
+        assertResults(null, "CODE_BLOCK(EXPRESSION(EXPRESSION ROUND(!a; PLUS; !b); PLUS; !c))",
+                ROUND_BRACKET_START_ID, "!a", PLUS_ID, "!b", ROUND_BRACKET_END_ID, PLUS_ID, "!c")
     }
 
     @Test
     fun programExceptionsTest() {
-        assertResults(createExceptions(TokenExpectedException("Token \"method\" expected!", null)),
-                null,
-                METHOD_ID, "!a", ROUND_BRACKET_START_ID, ROUND_BRACKET_END_ID, START_BLOCK_ID, END_BLOCK_ID, "!b")
-        assertResults(createExceptions(TokenExpectedException("Token \"begin\" expected!", null),
-                TokenExpectedException("Token \"method\" expected!", null)),
-                null,
-                METHOD_ID, "!a", ROUND_BRACKET_START_ID, ROUND_BRACKET_END_ID,
-                END_BLOCK_ID, NEWLINE_ID, "!c", "!d", NEWLINE_ID, "!e")
+        //TODO: more, updated tests
         assertResults(createExceptions(UnexpectedEndException("Token \"end\" expected!", null)),
                 null,
-                METHOD_ID, "!a", ROUND_BRACKET_START_ID, ROUND_BRACKET_END_ID, START_BLOCK_ID)
+            DECLARE_ID, "!a", ASSIGN_ID, METHOD_ID, ROUND_BRACKET_START_ID, ROUND_BRACKET_END_ID, START_BLOCK_ID)
         assertResults(createExceptions(UnexpectedEndException("Token \")\" expected!", null)),
                 null,
-                METHOD_ID, "!a", ROUND_BRACKET_START_ID)
-        assertResults(createExceptions(UnexpectedEndException("Expected method name!", null)),
-                null,
-                METHOD_ID)
-        assertResults(createExceptions(UnexpectedEndException("Token \"begin\" expected!", null),
-                UnexpectedEndException("Token \"method\" expected!", null)),
-                null,
-                METHOD_ID, "!name", ROUND_BRACKET_START_ID, ROUND_BRACKET_END_ID, END_BLOCK_ID, STATEMENT_TERMINATOR_ID,
-                METHOD_ID, "!name", ROUND_BRACKET_START_ID, ROUND_BRACKET_END_ID, STATEMENT_TERMINATOR_ID)
-        assertResults(createExceptions(UnexpectedTokenException("Cannot declare method in body-only mode!", null)),
-                null,
-                "!a", PLUS_ID, "!b", NEWLINE_ID,
-                METHOD_ID, "!main", ROUND_BRACKET_START_ID, ROUND_BRACKET_END_ID, START_BLOCK_ID, END_BLOCK_ID)
+            DECLARE_ID, "!a", ASSIGN_ID, METHOD_ID, ROUND_BRACKET_START_ID)
     }
 
     @Test
     fun methodTest() {
-        assertResults(null, "PROGRAM(METHOD main(TYPE INT32; CODE_BLOCK))",
-                METHOD_ID, "!main", ROUND_BRACKET_START_ID, ROUND_BRACKET_END_ID, COLON_ID, "!int",
+        assertResults(null, "CODE_BLOCK(DECLARATION(EXPRESSION(!main); METHOD(TYPE INT32; CODE_BLOCK)))",
+            DECLARE_ID, "!main", ASSIGN_ID, METHOD_ID, ROUND_BRACKET_START_ID, ROUND_BRACKET_END_ID, RIGHT_ARROW_ID, "!int",
                 START_BLOCK_ID, END_BLOCK_ID)
-        assertResults(null, "PROGRAM(METHOD main(TYPE INT32; DECLARATION(!a; TYPE INT32); CODE_BLOCK))",
-                METHOD_ID, "!main", ROUND_BRACKET_START_ID, "!a", COLON_ID, "!int", ROUND_BRACKET_END_ID,
-                COLON_ID, "!int", START_BLOCK_ID, END_BLOCK_ID)
-        assertResults(null, "PROGRAM(METHOD main(TYPE INT32; DECLARATION(!a; TYPE INT32); DECLARATION(!b; TYPE STRING); CODE_BLOCK))",
-                METHOD_ID, "!main", ROUND_BRACKET_START_ID, "!a", COLON_ID, "!int",
-                "COMMA", "!b", COLON_ID, "!text", ROUND_BRACKET_END_ID, COLON_ID, "!int", START_BLOCK_ID, END_BLOCK_ID)
-        assertResults(null, "PROGRAM(METHOD main(TYPE INT32; DECLARATION(!a; TYPE NOTHING); CODE_BLOCK))",
-                METHOD_ID, "!main", ROUND_BRACKET_START_ID, "!a", COLON_ID, NOTHING_ID, ROUND_BRACKET_END_ID,
-                COLON_ID, "!int", START_BLOCK_ID, END_BLOCK_ID)
-        assertResults(null, "PROGRAM(METHOD a(TYPE NOTHING; CODE_BLOCK))",
-                METHOD_ID, "!a", ROUND_BRACKET_START_ID, ROUND_BRACKET_END_ID, DO_ID, NOTHING_ID)
-        assertResults(null, "PROGRAM(METHOD main(TYPE NOTHING; CODE_BLOCK(EXPRESSION(!b; ASSIGN; #3))))",
-                METHOD_ID, "!main", ROUND_BRACKET_START_ID, ROUND_BRACKET_END_ID, DO_ID, "!b", ASSIGN_ID, "#3")
-        assertResults(null, "PROGRAM(METHOD product(TYPE INT64; DECLARATION(!a; TYPE INT32); DECLARATION(!b; TYPE INT32); CODE_BLOCK(EXPRESSION(!product; ASSIGN; !a; TIMES; !b))))",
-                METHOD_ID, "!product", ROUND_BRACKET_START_ID, "!a", COLON_ID, "!int",
-                COMMA_ID, "!b", COLON_ID, "!int", ROUND_BRACKET_END_ID, COLON_ID, "!bigint", NEWLINE_ID,
-                DO_ID, "!product", ASSIGN_ID, "!a", TIMES_ID, "!b")
-        assertResults(null,"PROGRAM(METHOD main(TYPE NOTHING; DECLARATION ANON(!arg1; TYPE INT32); CODE_BLOCK))",
-            METHOD_ID, "!main", ROUND_BRACKET_START_ID, "!arg1", COLON_ID, "!int", ROUND_BRACKET_END_ID, DO_ID, NOTHING_ID)
+        assertResults(null, "CODE_BLOCK(DECLARATION(EXPRESSION(!main); METHOD(TYPE INT32; PARAMETER(!a; TYPE INT32); CODE_BLOCK)))",
+            DECLARE_ID, "!main", ASSIGN_ID, METHOD_ID, ROUND_BRACKET_START_ID, "!a", COLON_ID, "!int", ROUND_BRACKET_END_ID,
+            RIGHT_ARROW_ID, "!int", START_BLOCK_ID, END_BLOCK_ID)
+        assertResults(null, "CODE_BLOCK(DECLARATION(EXPRESSION(!main); METHOD(TYPE INT32; PARAMETER(!a; TYPE INT32); PARAMETER(!b; TYPE STRING); CODE_BLOCK)))",
+            DECLARE_ID, "!main", ASSIGN_ID, METHOD_ID, ROUND_BRACKET_START_ID, "!a", COLON_ID, "!int",
+                "COMMA", "!b", COLON_ID, "!text", ROUND_BRACKET_END_ID, RIGHT_ARROW_ID, "!int", START_BLOCK_ID, END_BLOCK_ID)
+        assertResults(null, "CODE_BLOCK(DECLARATION(EXPRESSION(!main); METHOD(TYPE INT32; PARAMETER(!a; TYPE NOTHING); CODE_BLOCK)))",
+            DECLARE_ID, "!main", ASSIGN_ID, METHOD_ID, ROUND_BRACKET_START_ID, "!a", COLON_ID, NOTHING_ID, ROUND_BRACKET_END_ID,
+            RIGHT_ARROW_ID, "!int", START_BLOCK_ID, END_BLOCK_ID)
+        assertResults(null, "CODE_BLOCK(DECLARATION(EXPRESSION(!a); METHOD(TYPE NOTHING; CODE_BLOCK)))",
+            DECLARE_ID, "!a", ASSIGN_ID, METHOD_ID, ROUND_BRACKET_START_ID, ROUND_BRACKET_END_ID, DO_ID, NOTHING_ID)
+        assertResults(null, "CODE_BLOCK(DECLARATION(EXPRESSION(!main); METHOD(TYPE NOTHING; CODE_BLOCK(ASSIGNMENT(EXPRESSION(!b); EXPRESSION(#3))))))",
+            DECLARE_ID, "!main", ASSIGN_ID, METHOD_ID, ROUND_BRACKET_START_ID, ROUND_BRACKET_END_ID, DO_ID,
+            ASSIGNMENT_START_ID, "!b", ASSIGN_ID, "#3")
+        assertResults(null, "CODE_BLOCK(DECLARATION(EXPRESSION(!product); METHOD(TYPE INT64; PARAMETER(!a; TYPE INT32); PARAMETER(!b; TYPE INT32); CODE_BLOCK(ASSIGNMENT(EXPRESSION(!product); EXPRESSION(!a; TIMES; !b))))))",
+            DECLARE_ID, "!product", ASSIGN_ID, METHOD_ID, ROUND_BRACKET_START_ID, "!a", COLON_ID, "!int",
+                COMMA_ID, "!b", COLON_ID, "!int", ROUND_BRACKET_END_ID, RIGHT_ARROW_ID, "!bigint", NEWLINE_ID,
+                DO_ID, ASSIGNMENT_START_ID, "!product", ASSIGN_ID, "!a", TIMES_ID, "!b")
+        assertResults(null,"CODE_BLOCK(DECLARATION(EXPRESSION(!main); METHOD(TYPE NOTHING; PARAMETER ANON(!arg1; TYPE INT32); CODE_BLOCK)))",
+            DECLARE_ID, "!main", ASSIGN_ID, METHOD_ID, ROUND_BRACKET_START_ID, "!arg1", COLON_ID, "!int", ROUND_BRACKET_END_ID, DO_ID, NOTHING_ID)
     }
 
     @Test
     fun squareParenthesesTest() {
-        assertResults(null, "PROGRAM(METHOD main(TYPE NOTHING; CODE_BLOCK(EXPRESSION(!a; EXPRESSION SQUARE(!b; COMMA; !d; COMMA; !e; PLUS; #2; TIMES; EXPRESSION ROUND(!a; POWER; !b))))))",
+        assertResults(null, "CODE_BLOCK(EXPRESSION(!a; EXPRESSION SQUARE(!b; COMMA; !d; COMMA; !e; PLUS; #2; TIMES; EXPRESSION ROUND(!a; POWER; !b))))",
                 "!a", SQUARE_BRACKET_START_ID, "!b", COMMA_ID, "!d", COMMA_ID, "!e",
                 PLUS_ID, "#2", TIMES_ID, ROUND_BRACKET_START_ID, "!a", POWER_ID, "!b", ROUND_BRACKET_END_ID, SQUARE_BRACKET_END_ID)
-        assertResults(null, "PROGRAM(METHOD main(TYPE NOTHING; CODE_BLOCK(EXPRESSION(!a; EXPRESSION SQUARE(!b)))))",
+        assertResults(null, "CODE_BLOCK(EXPRESSION(!a; EXPRESSION SQUARE(!b)))",
                 "!a", SQUARE_BRACKET_START_ID, "!b", COMMA_ID, SQUARE_BRACKET_END_ID)
-        assertResults(null, "PROGRAM(METHOD main(TYPE NOTHING; CODE_BLOCK(EXPRESSION(!a; ASSIGN; EXPRESSION SQUARE(#3; COMMA; #4; COMMA; #5)))))",
-                "!a", ASSIGN_ID, SQUARE_BRACKET_START_ID, "#3", COMMA_ID, "#4", COMMA_ID, "#5", COMMA_ID, SQUARE_BRACKET_END_ID)
-        assertResults(null, "PROGRAM(METHOD main(TYPE NOTHING; CODE_BLOCK(EXPRESSION(!a; ASSIGN; !b; EXPRESSION SQUARE(!c; COMMA; !d; EXPRESSION SQUARE(!e)); PLUS; !f))))",
-                "!a", ASSIGN_ID, "!b", SQUARE_BRACKET_START_ID, "!c", COMMA_ID, "!d",
+        assertResults(null, "CODE_BLOCK(ASSIGNMENT(EXPRESSION(!a); EXPRESSION(!b; EXPRESSION SQUARE(#3; COMMA; #4; COMMA; #5))))",
+                ASSIGNMENT_START_ID,"!a", ASSIGN_ID,"!b", SQUARE_BRACKET_START_ID, "#3", COMMA_ID, "#4", COMMA_ID, "#5", COMMA_ID, SQUARE_BRACKET_END_ID)
+        assertResults(null, "CODE_BLOCK(ASSIGNMENT(EXPRESSION(!a); EXPRESSION(!b; EXPRESSION SQUARE(!c; COMMA; !d; EXPRESSION SQUARE(!e)); PLUS; !f)))",
+                ASSIGNMENT_START_ID,"!a", ASSIGN_ID, "!b", SQUARE_BRACKET_START_ID, "!c", COMMA_ID, "!d",
                 SQUARE_BRACKET_START_ID, "!e", SQUARE_BRACKET_END_ID, SQUARE_BRACKET_END_ID, PLUS_ID, "!f")
     }
 
