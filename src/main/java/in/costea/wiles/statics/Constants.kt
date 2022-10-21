0e36f85384cee3c006d1f29009ed9b84d0a942a8
Alex Costea <alex@costea.in>
2022-10-21 16:27:28 +0300

    keyword identifiers support

--- src/main/java/in/costea/wiles/statics/Constants.kt
+++ src/main/java/in/costea/wiles/statics/Constants.kt
@@ -54,21 +54,17 @@ object Constants {
     const val NOT_ID = "NOT"
     private const val MAYBE_ID = "MAYBE"
     const val RETURN_ID = "RETURN"
-
+    private const val TRUE_ID = "TRUE"
+    private const val FALSE_ID = "FALSE"
+    private const val IF_ID = "IF"
+    private const val WHEN_ID = "WHEN"
+    private const val ELSE_ID = "ELSE"
+    private const val BREAK_ID = "BREAK"
+    private const val CONTINUE_ID = "CONTINUE"
+    private const val FOR_ID = "FOR"
+    private const val WHILE_ID = "WHILE"
     const val UNNAMED_START = IDENTIFIER_START + "arg"
     const val INTERNAL_ERROR = "Internal error!"
-
-    private val KEYWORDS: BiMap<String, String> = HashBiMap.create()
-
-    private val SYMBOLS: BiMap<String, String> = HashBiMap.create()
-
-    @JvmField
-    val TYPES: BiMap<String, String> = HashBiMap.create()
-    @JvmField
-    val TOKENS: BiMap<String, String>
-
-    @JvmField
-    val TOKENS_INVERSE: BiMap<String, String>
     const val MAX_SYMBOL_LENGTH = 3
     const val STRING_DELIMITER = '"'
     const val DECIMAL_DELIMITER = '.'
@@ -78,40 +74,44 @@ object Constants {
     const val SPACE = ' '
     const val CONTINUE_LINE = '\\'
 
+    private val KEYWORDS: BiMap<String, String> = HashBiMap.create()
+    private val SYMBOLS: BiMap<String, String> = HashBiMap.create()
+    @JvmField
+    val TYPES: BiMap<String, String> = HashBiMap.create()
+    @JvmField
+    val TOKENS: BiMap<String, String>
+    @JvmField
+    val TOKENS_INVERSE: BiMap<String, String>
+    val PRECEDENCE : HashMap<String,Byte> = HashMap()
+    val RIGHT_TO_LEFT : Set<Byte>
+
     @JvmField
     val INFIX_OPERATORS = setOf(
         PLUS_ID, MINUS_ID, TIMES_ID, DIVIDE_ID, POWER_ID,
         EQUALS_ID, LARGER_ID, SMALLER_ID, LARGER_EQUALS_ID, SMALLER_EQUALS_ID, NOT_EQUAL_ID,
         ACCESS_ID, SEPARATOR_ID, AND_ID, OR_ID, APPLY_ID
     )
-
     @JvmField
     val PREFIX_OPERATORS = setOf(UNARY_PLUS_ID, UNARY_MINUS_ID, NOT_ID)
-
     @JvmField
     val STARTING_OPERATORS = setOf(PLUS_ID, MINUS_ID, NOT_ID)
-
-    val PRECEDENCE : HashMap<String,Byte> = HashMap()
-
     @JvmField
     val ROUND_BRACKETS = setOf(ROUND_BRACKET_START_ID, ROUND_BRACKET_END_ID)
-
     @JvmField
     val TERMINATORS = setOf(NEWLINE_ID, TERMINATOR_ID)
+    private val KEYWORD_LITERALS = setOf(TRUE_ID,FALSE_ID,NOTHING_ID)
+
+    @JvmField
+    val IS_IDENTIFIER = Predicate { x: String -> x.length > 1 && x.startsWith(IDENTIFIER_START) }
+    private val IS_TEXT_LITERAL = Predicate { x: String -> x.length > 1 && x.startsWith(STRING_START) }
+    private val IS_NUMBER_LITERAL = Predicate { x: String -> x.length > 1 && x.startsWith(NUM_START) }
+    private val IS_KEYWORD_LITERAL= Predicate { x:String -> KEYWORD_LITERALS.contains(x) }
+    @JvmField
+    val IS_LITERAL: Predicate<String> = IS_IDENTIFIER.or(IS_TEXT_LITERAL).or(IS_NUMBER_LITERAL).or(IS_KEYWORD_LITERAL)
 
     @JvmField
     val EXPECT_TERMINATOR = tokenOf(isContainedIn(TERMINATORS))
         .dontIgnoreNewLine().withErrorMessage("End of line or semicolon expected!")
-
-    @JvmField
-    val IS_IDENTIFIER = Predicate { x: String -> x.length > 1 && x.startsWith(IDENTIFIER_START) || x == NOTHING_ID }
-    private val IS_TEXT_LITERAL = Predicate { x: String -> x.length > 1 && x.startsWith(STRING_START) }
-    private val IS_NUMBER_LITERAL = Predicate { x: String -> x.length > 1 && x.startsWith(NUM_START) }
-
-    @JvmField
-    val IS_LITERAL: Predicate<String> = IS_IDENTIFIER.or(IS_TEXT_LITERAL).or(IS_NUMBER_LITERAL)
-
-    val RIGHT_TO_LEFT : Set<Byte>
 
     init {
         PRECEDENCE[OR_ID] = -4
@@ -139,20 +139,22 @@ object Constants {
         KEYWORDS["method"] = METHOD_ID
         KEYWORDS["let"] = DECLARE_ID
         KEYWORDS["var"] = MUTABLE_ID
-        KEYWORDS["if"] = "IF"
-        KEYWORDS["when"] = "WHEN"
-        KEYWORDS["otherwise"] = "ELSE"
-        KEYWORDS["for"] = "FOR"
-        KEYWORDS["while"] = "WHILE"
+        KEYWORDS["if"] = IF_ID
+        KEYWORDS["when"] = WHEN_ID
+        KEYWORDS["otherwise"] = ELSE_ID
+        KEYWORDS["for"] = FOR_ID
+        KEYWORDS["while"] = WHILE_ID
         KEYWORDS["and"] = AND_ID
         KEYWORDS["or"] = OR_ID
         KEYWORDS["not"] = NOT_ID
-        KEYWORDS["stop"] = "BREAK"
-        KEYWORDS["skip"] = "CONTINUE"
+        KEYWORDS["stop"] = BREAK_ID
+        KEYWORDS["skip"] = CONTINUE_ID
         KEYWORDS["yield"] = RETURN_ID
         KEYWORDS["do"] = DO_ID
         KEYWORDS["begin"] = START_BLOCK_ID
         KEYWORDS["end"] = END_BLOCK_ID
+        KEYWORDS["true"] = TRUE_ID
+        KEYWORDS["false"] = FALSE_ID
 
         TYPES["!bit"] = "BOOLEAN"
         TYPES["!byte"] = "INT8"
@@ -164,7 +166,7 @@ object Constants {
         TYPES["!list"] = "ARRAY_LIST"
         TYPES["!either"] = "EITHER"
         TYPES["!range"] = "RANGE"
-        TYPES[NOTHING_ID] = "NOTHING"
+        TYPES[NOTHING_ID] = NOTHING_ID
 
         SYMBOLS["+"] = PLUS_ID
         SYMBOLS["-"] = MINUS_ID
