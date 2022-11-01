4a0863b8b14d218a7bea0beb586e09611ceeeb01
Alex Costea <alex@costea.in>
2022-11-01 10:50:18 +0200

    type improvements

--- src/main/java/in/costea/wiles/constants/Types.kt
+++ src/main/java/in/costea/wiles/constants/Types.kt
@@ -6,21 +6,22 @@ import `in`.costea.wiles.constants.Settings.ROMANIAN_MODE
 import `in`.costea.wiles.constants.Tokens.MAYBE_ID
 import `in`.costea.wiles.constants.Tokens.METHOD_ID
 import `in`.costea.wiles.constants.Tokens.NOTHING_ID
-import `in`.costea.wiles.constants.Tokens.TIMES_ID
 
 object Types {
     @JvmField
     val TYPES: BiMap<String, String> = HashBiMap.create()
 
-    const val BOOLEAN_ID = "BOOLEAN"
-    const val INT8_ID = "INT8"
-    const val INT16_ID = "INT16"
-    const val INT32_ID = "INT32"
-    const val INT64_ID = "INT64"
-    const val STRING_ID = "STRING"
-    const val DOUBLE_ID = "DOUBLE"
-    const val LIST_ID = "LIST"
+    private const val BOOLEAN_ID = "BOOLEAN"
+    private const val INT8_ID = "INT8"
+    private const val INT16_ID = "INT16"
+    private const val INT32_ID = "INT32"
+    private const val INT64_ID = "INT64"
+    private const val STRING_ID = "STRING"
+    private const val DOUBLE_ID = "DOUBLE"
+    private const val LIST_ID = "LIST"
+    private const val ANYTHING_ID = "ANYTHING"
     const val GENERIC_ID = "GENERIC"
+
 
     val REQUIRES_SUBTYPE = setOf(LIST_ID)
 
@@ -33,7 +34,8 @@ object Types {
         TYPES["!text"] = STRING_ID
         TYPES[if(!ROMANIAN_MODE) "!rational" else "!rațional"] = DOUBLE_ID
         TYPES[if(!ROMANIAN_MODE) "!list" else "!listă"] = LIST_ID
-        TYPES[TIMES_ID] =  GENERIC_ID
+        TYPES[if(!ROMANIAN_MODE) "!anything" else "!orice"] = ANYTHING_ID
+        TYPES["!"] =  GENERIC_ID
         TYPES[NOTHING_ID] = NOTHING_ID
         TYPES[MAYBE_ID] = MAYBE_ID
         TYPES[METHOD_ID] = METHOD_ID
