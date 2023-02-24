1020e429ff1842c01c568d09b59d9bb11c31c603
Alex Costea <alex@costea.in>
2023-02-24 23:24:55 +0200

    bug fix

--- src/main/java/wiles/checker/inferrers/InferFromType.kt
+++ src/main/java/wiles/checker/inferrers/InferFromType.kt
@@ -1,8 +1,11 @@
 package wiles.checker.inferrers
 
+import wiles.checker.CheckerConstants.NOTHING_TYPE
 import wiles.checker.InferrerDetails
 import wiles.shared.SyntaxType
 import wiles.shared.constants.Tokens.METHOD_ID
+import wiles.shared.constants.Tokens.MUTABLE_ID
+import wiles.shared.constants.Types.EITHER_ID
 
 class InferFromType(details: InferrerDetails) : InferFromStatement(details) {
     override fun infer() {
@@ -15,6 +18,14 @@ class InferFromType(details: InferrerDetails) : InferFromStatement(details) {
                 {
                     InferFromDeclaration(InferrerDetails(component, variables.copy(), exceptions)).infer()
                 }
+            }
+            method.components.add(0,NOTHING_TYPE)
+        }
+        else if(statement.name == EITHER_ID || statement.name == MUTABLE_ID)
+        {
+            for(component in statement.components)
+            {
+                InferFromType(InferrerDetails(component,variables,exceptions)).infer()
             }
         }
     }
