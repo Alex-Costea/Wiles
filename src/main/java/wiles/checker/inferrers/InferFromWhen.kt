150bce5e686c3bb0fca760e5598e61a14a067c96
Alex Costea <alex@costea.in>
2023-02-27 19:49:32 +0200

    check if initialized with when

--- src/main/java/wiles/checker/inferrers/InferFromWhen.kt
+++ src/main/java/wiles/checker/inferrers/InferFromWhen.kt
@@ -1,6 +1,8 @@
 package wiles.checker.inferrers
 
 import wiles.checker.data.InferrerDetails
+import wiles.checker.data.VariableDetails
+import wiles.checker.data.VariableMap
 import wiles.checker.exceptions.ConflictingTypeDefinitionException
 import wiles.checker.services.InferrerService
 import wiles.checker.statics.CheckerConstants.BOOLEAN_TYPE
@@ -9,6 +11,7 @@ import wiles.checker.statics.InferrerUtils
 class InferFromWhen(details: InferrerDetails) : InferFromStatement(details) {
     override fun infer() {
         val components = statement.components.toMutableList()
+        val listOfVariableMaps = mutableListOf<VariableMap>()
         while(components.isNotEmpty())
         {
             val condition = components.removeFirst()
@@ -22,7 +25,25 @@ class InferFromWhen(details: InferrerDetails) : InferFromStatement(details) {
             }
 
             val block = components.removeFirst()
-            InferFromCodeBlock(InferrerDetails(block, variables.copy(), exceptions, additionalVars)).infer()
+            val newVars = variables.copy()
+            InferFromCodeBlock(InferrerDetails(block, newVars, exceptions, additionalVars)).infer()
+            listOfVariableMaps.add(newVars)
+        }
+
+        for(variable in variables.entries)
+        {
+            if(!variable.value.initialized)
+            {
+                var isInitialized = true
+                for(map in listOfVariableMaps)
+                {
+                    if(!map[variable.key]!!.initialized)
+                    {
+                        isInitialized = false
+                    }
+                }
+                variables[variable.key]= VariableDetails(variable.value.type,isInitialized,variable.value.modifiable)
+            }
         }
     }
 }
