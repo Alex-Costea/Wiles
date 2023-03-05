efbf005e77a2d70842615d3e9a6136025bb614db
Alex Costea <alex@costea.in>
2023-03-05 09:00:43 +0200

    clone value

--- src/main/java/wiles/interpreter/data/ObjectDetails.kt
+++ src/main/java/wiles/interpreter/data/ObjectDetails.kt
@@ -1,12 +1,25 @@
 package wiles.interpreter.data
 
+import wiles.shared.InternalErrorException
 import wiles.shared.JSONStatement
 
 data class ObjectDetails(val value : Any?, val type : JSONStatement)
 {
+
+    private fun cloneValue(value : Any?) : Any?
+    {
+        val newValue = when(value)
+        {
+            is Double, is Long, is String, is Boolean, null -> value
+            is MutableList<*> -> value.map{cloneValue(it)}.toMutableList()
+            is JSONStatement -> value.copyRemovingLocation()
+            else -> throw InternalErrorException()
+        }
+        return newValue
+    }
+
     fun clone() : ObjectDetails
     {
-        //TODO: deep copy for value?
-        return ObjectDetails(this.value, this.type.copyRemovingLocation())
+        return ObjectDetails(cloneValue(value), type.copyRemovingLocation())
     }
 }
