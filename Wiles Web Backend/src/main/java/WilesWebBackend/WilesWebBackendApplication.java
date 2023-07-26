8e2e57c8f67b6faf60fe5fa445518e8c7755da44
Alex Costea <alex@costea.in>
2023-07-26 15:54:45 +0300

    use HTTP response codes

--- Wiles Web Backend/src/main/java/WilesWebBackend/WilesWebBackendApplication.java
+++ Wiles Web Backend/src/main/java/WilesWebBackend/WilesWebBackendApplication.java
@@ -3,7 +3,9 @@ package WilesWebBackend;
 import kotlin.Pair;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -25,10 +27,10 @@ public class WilesWebBackendApplication {
 
 	@RequestMapping(value = "/run", method = RequestMethod.POST,
 			produces = MediaType.APPLICATION_JSON_VALUE)
-	public CompilationResponse compile(@RequestBody Map<String, Object> payload) {
+	public ResponseEntity<CompilationResponse> compile(@RequestBody Map<String, Object> payload) {
 		String code = (String) payload.getOrDefault("code",null);
 		if(code == null)
-			return new CompilationResponse(null,"Code not provided!");
+			return ResponseEntity.badRequest().body(new CompilationResponse(null,"Code not provided!"));
 		String input = (String) payload.getOrDefault("input","");
 
 		ArrayList<String> args = new ArrayList<>();
@@ -44,11 +46,12 @@ public class WilesWebBackendApplication {
 			String errorsText = result.getSecond();
 			if(Objects.equals(errorsText, ""))
 				errorsText = null;
-			return new CompilationResponse(outputText, errorsText);
+			return ResponseEntity.ok(new CompilationResponse(outputText, errorsText));
 		}
 		catch (TimeoutException | InterruptedException | ExecutionException e)
 		{
-			return new CompilationResponse("", "Interpreter timed out!");
+			return ResponseEntity.status(HttpStatus.GATEWAY_TIMEOUT)
+					.body(new CompilationResponse("", "Interpreter timed out!"));
 		}
 		finally {
 			executor.shutdownNow();
