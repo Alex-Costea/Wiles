05747b4f99d9f13c3047a077294bfaeb04b4504a
Alex Costea <alex@costea.in>
2023-08-13 18:22:43 +0300

    better security

--- Wiles Web Backend/src/main/java/WilesWebBackend/SpringSecurityConfig.java
+++ Wiles Web Backend/src/main/java/WilesWebBackend/SpringSecurityConfig.java
@@ -4,14 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.channel.ChannelProcessingFilter;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @ComponentScan(basePackages = "WilesWebBackend")
 @Configuration
--- Wiles Web Backend/src/main/java/WilesWebBackend/WilesWebBackendApplication.java
+++ Wiles Web Backend/src/main/java/WilesWebBackend/WilesWebBackendApplication.java
@@ -3,12 +3,14 @@ package WilesWebBackend;
 import kotlin.Pair;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.Map;
