ae0a9e2fa7a6c968ac850d3065fd211ff97f81c5
Alex Costea <alex@costea.in>
2023-08-20 22:56:32 +0300

    HTTP fix

--- wiles-web-frontend/src/app/app.component.ts
+++ wiles-web-frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 
@@ -13,13 +13,18 @@ export class AppComponent implements OnInit {
   ngOnInit()
   {
     //workaround to load CSFR on start
-    this.http.put<any>("run",this.myForm.value, { withCredentials: true}).subscribe().unsubscribe()
+    const headers = new HttpHeaders({
+      'Cache-Control':  'no-cache, no-store, must-revalidate, post- check=0, pre-check=0',
+      'Pragma': 'no-cache',
+      'Expires': '0'
+    });
+    this.http.put<any>("run",this.myForm.value, { withCredentials: true, headers : headers}).subscribe().unsubscribe()
   }
 
 
   onSubmit()
   {
-    this.http.put<any>("/run",this.myForm.value, { withCredentials: true}).subscribe(data =>
+    this.http.put<any>("/run",this.myForm.value, { withCredentials: true }).subscribe(data =>
       {
         let response : string = data.response;
         let errors : string | null = data.errors;
