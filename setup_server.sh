ae92a8b330d1fd1cfc953906dec06151c20a26fa
Alex Costea <alex@costea.in>
2023-08-20 22:58:35 +0300

    script fix

--- setup_server.sh
+++ setup_server.sh
@@ -1,5 +1,5 @@
 rm -d -r "wiles-web-frontend/dist/wiles-web-frontend/"
-rm -d -r "Wiles Web Backend/src/main/resources/online"
+rm -d -r "Wiles Web Backend/src/main/resources/static"
 cd wiles-web-frontend
 ng build --base-href /
 cd ..
