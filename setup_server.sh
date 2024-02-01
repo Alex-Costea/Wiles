368c6349d45369ad11b599073174513acee68be1
Alex Costea <alex@costea.in>
2024-02-01 14:21:45 +0200

    remove Angular, add React

--- setup_server.sh
+++ setup_server.sh
@@ -1,6 +1,6 @@
-rm -d -r "wiles-web-frontend/dist/wiles-web-frontend/"
+rm -d -r "wiles-web-frontend/build/"
 rm -d -r "Wiles Web Backend/src/main/resources/static"
 cd wiles-web-frontend
-ng build --base-href /
+npm run build
 cd ..
-cp -R "wiles-web-frontend/dist/wiles-web-frontend/" "Wiles Web Backend/src/main/resources/static/"
+cp -R "wiles-web-frontend/build/" "Wiles Web Backend/src/main/resources/static/"
