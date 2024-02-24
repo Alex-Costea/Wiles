13ead3c9bc8cfe064c5ecbaaa7e4e31907e8ab2f
Alex Costea <alex@costea.in>
2024-02-24 14:38:21 +0200

    update reboot.sh

--- reboot.sh
+++ reboot.sh
@@ -2,4 +2,6 @@ sudo killall -9 java
 git pull
 sh setup_server.sh
 cd "Wiles Web Backend/target"
-sudo nohup java -jar -Dspring.profiles.active=prod Wiles-Web-Backend-0.0.1-SNAPSHOT.jar  &
+sudo -s
+nohup java -jar -Dspring.profiles.active=prod Wiles-Web-Backend-0.0.1-SNAPSHOT.jar  &
+exit
