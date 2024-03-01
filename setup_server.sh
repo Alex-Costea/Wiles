a7b7683cb7eac76d77b126f486b85ab6af1ad24e
Alex Costea <alex@costea.in>
2024-03-01 22:48:09 +0200

    Update `setup_server.sh`

--- setup_server.sh
+++ setup_server.sh
@@ -1,6 +1,7 @@
 rm -d -r "wiles-web-frontend/build/"
 rm -d -r "wiles-web-backend/src/main/resources/static"
 cd wiles-web-frontend
+npm install
 npm run build
 cd ..
 cp -R "wiles-web-frontend/build/" "wiles-web-backend/src/main/resources/static/"
