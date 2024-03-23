d47a55146b542e07fa9225b09327da6840728d3c
Alex Costea <alex@costea.in>
2024-03-23 18:27:33 +0200

    Remove nohup

--- scripts/reboot.sh
+++ scripts/reboot.sh
@@ -3,4 +3,4 @@ sudo killall -9 java
 git pull
 sh setup_server.sh
 cd "wiles-web-backend/target"
-sudo --preserve-env=PATH env nohup java -jar -Dspring.profiles.active=prod Wiles-Web-Backend-0.0.1-SNAPSHOT.jar  &
+sudo --preserve-env=PATH env java -jar -Dspring.profiles.active=prod Wiles-Web-Backend-0.0.1-SNAPSHOT.jar
