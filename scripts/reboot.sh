db4f697d11cd5d5330acb3e9668a53c5ba07b023
Alex Costea <alex@costea.in>
2024-03-23 18:43:31 +0200

    Script fix

--- scripts/reboot.sh
+++ scripts/reboot.sh
@@ -1,6 +1,6 @@
-cd ..
 sudo killall -9 java
 git pull
 sh setup_server.sh
+cd ..
 cd "wiles-web-backend/target"
 sudo --preserve-env=PATH env java -jar -Dspring.profiles.active=prod Wiles-Web-Backend-0.0.1-SNAPSHOT.jar
