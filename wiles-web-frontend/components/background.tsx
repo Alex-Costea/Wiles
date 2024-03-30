192655eada8f578d9ca1ad2dd8f0f7ad9cfc7083
Alex Costea <alex@costea.in>
2024-03-30 14:47:11 +0200

    Get image from external link

--- wiles-web-frontend/components/background.tsx
+++ wiles-web-frontend/components/background.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 function Background()
 {
     return <div className="background">
-        <Image src="images/background.png"
+        <Image src="https://raw.githubusercontent.com/Alex-Costea/Wiles/2f2aca84996fd8b3d77bc337d4576dcf306b1891/wiles-web-frontend/public/images/background.png"
                alt="background" fill={true} priority style={{objectFit: "cover"}}></Image>
     </div>
 }
