0a6c5712b9c562a69569feadb3a1415cfb075856
Alex Costea <alex@costea.in>
2024-07-13 15:16:14 +0300

    Fix setup_server.sh

--- scripts/setup_server.sh
+++ scripts/setup_server.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 ./compile_base.sh
 cd ..
-rm -d -r wiles-web-frontend/build/
+rm -d -r wiles-web-frontend/dist/
 rm -d -r wiles-web-backend/src/main/resources/static
 cd wiles-web-frontend  || exit
 npm install
 npm run build
 cd ..
-cp -R wiles-web-frontend/build/ wiles-web-backend/src/main/resources/static/
+cp -R wiles-web-frontend/dist/ wiles-web-backend/src/main/resources/static/
 cd wiles-web-backend  || exit
 mvn clean install
