fb68d0a8678ffb7d4ccdb912282930bcfdf5268a
Alex Costea <alex@costea.in>
2024-07-18 14:06:06 +0300

    paste fix

--- wiles-web-frontend/src/field/field.tsx
+++ wiles-web-frontend/src/field/field.tsx
@@ -11,12 +11,22 @@ interface FieldProps {
 
 function Field({id, label = "", innerHTML, onChange = () => {}, disabled = false} : FieldProps)
 {
+    function onPaste(e : React.ClipboardEvent) {
+        e.preventDefault()
+        const text = e.clipboardData?.getData("text/plain")
+        const selection = window.getSelection();
+        if (!selection?.rangeCount) return;
+        selection.deleteFromDocument();
+        selection.getRangeAt(0).insertNode(document.createTextNode(text));
+        selection.collapseToEnd();
+    }
+
     return <React.Fragment>
         <p>
             <label htmlFor={id}>{label}</label>
         </p>
         <p className={"fieldOuter"}>
-            <ContentEditable id={id} className={"field"} tagName={"span"} spellCheck={false}
+            <ContentEditable id={id} className={"field"} tagName={"span"} spellCheck={false} onPaste={onPaste}
                              onChange={onChange} html={innerHTML} disabled={disabled}/>
         </p>
     </React.Fragment>
