d4d0645b87adc6ecdaae2a64e54197d291fd8189
Alex Costea <alex@costea.in>
2025-02-07 21:13:52 -0500

    Infinity literal

--- wiles-base/src/main/java/wiles/shared/constants/Tokens.kt
+++ wiles-base/src/main/java/wiles/shared/constants/Tokens.kt
@@ -82,6 +82,7 @@ object Tokens {
     const val CONST_TYPE_ID = "!constant"
     const val RANGE_ID = "!range"
     const val TYPE_ID = "!type"
+    const val INFINITY_ID = "!Infinity"
 
     private val KEYWORDS: HashMap<String, String> = HashMap()
     private val SYMBOLS: HashMap<String, String> = HashMap()
@@ -146,6 +147,7 @@ object Tokens {
         KEYWORDS["constant"] = CONST_TYPE_ID
         KEYWORDS["range"] = RANGE_ID
         KEYWORDS["type"] = TYPE_ID
+        KEYWORDS["Infinity"] = INFINITY_ID
 
         SYMBOLS["+"] = PLUS_ID
         SYMBOLS["-"] = MINUS_ID
