5f843213ab3e72165ffc2e159644ddd73c351ea5
Alex Costea <alex@costea.in>
2025-02-21 11:43:34 -0500

    refactor

--- wiles-base/src/main/java/wiles/processor/operations/AbstractOperation.kt
+++ wiles-base/src/main/java/wiles/processor/operations/AbstractOperation.kt
@@ -9,6 +9,7 @@ abstract class AbstractOperation(val left : Value, val right : Value, val contex
     val rightType = right.getType()
     val leftObj = left.getObj()
     val rightObj = right.getObj()
+    val bothKnown = left.isKnown() && right.isKnown()
     abstract fun getNewValue() : Value
     protected abstract fun calculateObject() : Any?
     protected abstract fun calculateType() : AbstractType
--- wiles-base/src/main/java/wiles/processor/operations/PlusOperation.kt
+++ wiles-base/src/main/java/wiles/processor/operations/PlusOperation.kt
@@ -12,27 +12,29 @@ import java.math.BigInteger
 
 class PlusOperation(left: Value, right: Value, context: InterpreterContext) : AbstractOperation(left, right, context) {
 
-    override fun calculateObject() : Any
+    override fun calculateObject() : Any?
     {
+        if(!bothKnown)
+            return null
         if(leftObj is BigInteger && rightObj is BigInteger) return leftObj + rightObj
         return leftObj.toString() + rightObj.toString()
     }
 
     override fun calculateType(): AbstractType {
         //TODO: handle sum types
-        return if (leftType.typeName == WilesTypes.INT && rightType.typeName == WilesTypes.INT) IntegerType()
-        else if (leftType.typeName == WilesTypes.STRING || rightType.typeName == WilesTypes.STRING) StringType()
-        else TODO("Can't add these types")
+        val newType = when {
+            leftType.typeName == WilesTypes.INT && rightType.typeName == WilesTypes.INT -> IntegerType()
+            leftType.typeName == WilesTypes.STRING || rightType.typeName == WilesTypes.STRING -> StringType()
+            else -> TODO("Can't add these types")
+        }
+        return newType
     }
 
     override fun getNewValue(): Value {
+        val newObject = calculateObject()
         val newType = calculateType()
-        if(left.isKnown() && right.isKnown())
-        {
-            val newVal = calculateObject()
-            newType.singletonValueOf(newVal)
-            return Value(newVal, newType, VariableStatus.Val, KnownStatus.Known)
-        }
-        else return Value(null, newType, VariableStatus.Val, KnownStatus.Unknown)
+        if(bothKnown)
+            newType.singletonValueOf(newObject!!)
+        return Value(newObject, newType, VariableStatus.Val, if(bothKnown) KnownStatus.Known else KnownStatus.Unknown)
     }
 }
