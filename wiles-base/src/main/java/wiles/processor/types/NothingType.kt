53276f6da1eaf64decfadd61c2a1225c2596e216
Alex Costea <alex@costea.in>
2025-03-05 13:09:16 -0500

    refactor NothingType

--- wiles-base/src/main/java/wiles/processor/types/NothingType.kt
+++ wiles-base/src/main/java/wiles/processor/types/NothingType.kt
@@ -2,11 +2,11 @@ package wiles.processor.types
 
 import wiles.processor.enums.WilesTypes
 
-class NothingType(exactValue: Any? = null) : AbstractType(exactValue) {
+class NothingType : AbstractType(null) {
     override val typeName: WilesTypes
         get() = WilesTypes.Nothing
 
     override fun clone(value: Any?): AbstractType {
-        return NothingType(value)
+        return NothingType()
     }
 }
