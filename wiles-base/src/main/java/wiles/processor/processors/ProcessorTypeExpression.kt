91b26b7817e2c6a4fd01f1b41473af3738c8305f
Alex Costea <alex@costea.in>
2025-03-05 13:07:37 -0500

    refactor

--- wiles-base/src/main/java/wiles/processor/processors/ProcessorTypeExpression.kt
+++ wiles-base/src/main/java/wiles/processor/processors/ProcessorTypeExpression.kt
@@ -4,34 +4,13 @@ import wiles.processor.data.InterpreterContext
 import wiles.processor.data.Value
 import wiles.processor.enums.VariableStatus
 import wiles.processor.errors.ValueNotConstException
-import wiles.processor.types.AbstractType
-import wiles.processor.types.AbstractType.Companion.BOOLEAN_TYPE
-import wiles.processor.types.AbstractType.Companion.DECIMAL_TYPE
-import wiles.processor.types.AbstractType.Companion.INTEGER_TYPE
-import wiles.processor.types.AbstractType.Companion.NOTHING_TYPE
-import wiles.processor.types.AbstractType.Companion.TEXT_TYPE
 import wiles.processor.types.AbstractType.Companion.TYPE_TYPE
-import wiles.processor.values.WilesDecimal
-import wiles.processor.values.WilesInteger
-import wiles.processor.values.WilesNothing
+import wiles.processor.utils.TypeUtils.getNewTypeObject
 import wiles.shared.abstracts.AbstractSyntaxTree
 
 class ProcessorTypeExpression(syntax: AbstractSyntaxTree, context: InterpreterContext)
     : ProcessorExpression(syntax, context)
 {
-
-    private fun getNewTypeObject(value : Value) : AbstractType{
-        return when(val obj = value.getObj()) {
-            is WilesInteger -> INTEGER_TYPE.exactly(obj)
-            is WilesDecimal -> DECIMAL_TYPE.exactly(obj)
-            is WilesNothing -> NOTHING_TYPE
-            is String -> TEXT_TYPE.exactly(obj)
-            is Boolean -> BOOLEAN_TYPE.exactly(obj)
-            is AbstractType -> obj
-            else -> TODO("Rest of values")
-        }
-    }
-
     override fun process() {
         if(syntax.getComponents().size > 1)
             super.process()
--- wiles-base/src/main/java/wiles/processor/utils/TypeUtils.kt
+++ wiles-base/src/main/java/wiles/processor/utils/TypeUtils.kt
@@ -1,7 +1,17 @@
 package wiles.processor.utils
 
+import wiles.processor.data.Value
 import wiles.processor.enums.WilesTypes
 import wiles.processor.types.AbstractType
+import wiles.processor.types.AbstractType.Companion.BOOLEAN_TYPE
+import wiles.processor.types.AbstractType.Companion.DECIMAL_TYPE
+import wiles.processor.types.AbstractType.Companion.INTEGER_TYPE
+import wiles.processor.types.AbstractType.Companion.NOTHING_TYPE
+import wiles.processor.types.AbstractType.Companion.TEXT_TYPE
+import wiles.processor.values.WilesDecimal
+import wiles.processor.values.WilesInteger
+import wiles.processor.values.WilesNothing
+import wiles.shared.errors.InternalErrorException
 
 object TypeUtils {
     private fun checkExactStatus(former : AbstractType, latter : AbstractType): Boolean {
@@ -21,4 +31,17 @@ object TypeUtils {
             else -> false
         }
     }
+
+    fun getNewTypeObject(value : Value) : AbstractType{
+        return when(val obj = value.getObj()) {
+            is WilesInteger -> INTEGER_TYPE.exactly(obj)
+            is WilesDecimal -> DECIMAL_TYPE.exactly(obj)
+            is WilesNothing -> NOTHING_TYPE
+            is String -> TEXT_TYPE.exactly(obj)
+            is Boolean -> BOOLEAN_TYPE.exactly(obj)
+            is AbstractType -> obj
+            else -> throw InternalErrorException()
+        }
+    }
+
 }
